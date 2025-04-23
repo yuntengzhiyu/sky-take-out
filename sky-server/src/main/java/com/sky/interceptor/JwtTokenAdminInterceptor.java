6566411be371c7735ca1ebc522d52b09a7bf102c
云腾致雨 <shenmafuyun213@163.com>
2025-04-23 16:06:08 +0800

    苍穹外卖init工程

--- sky-server/src/main/java/com/sky/interceptor/JwtTokenAdminInterceptor.java
+++ sky-server/src/main/java/com/sky/interceptor/JwtTokenAdminInterceptor.java
@@ -1,6 +1,7 @@
 package com.sky.interceptor;
 
 import com.sky.constant.JwtClaimsConstant;
+import com.sky.context.BaseContext;
 import com.sky.properties.JwtProperties;
 import com.sky.utils.JwtUtil;
 import io.jsonwebtoken.Claims;
@@ -45,8 +46,11 @@ public class JwtTokenAdminInterceptor implements HandlerInterceptor {
         try {
             log.info("jwt校验:{}", token);
             Claims claims = JwtUtil.parseJWT(jwtProperties.getAdminSecretKey(), token);
+//            Long empId = Long.valueOf(claims.get(JwtClaimsConstant.EMP_ID).toString());
+            //动态获取员工的ID并存到threadlocal中
             Long empId = Long.valueOf(claims.get(JwtClaimsConstant.EMP_ID).toString());
             log.info("当前员工id：", empId);
+            BaseContext.setCurrentId(empId);
             //3、通过，放行
             return true;
         } catch (Exception ex) {
