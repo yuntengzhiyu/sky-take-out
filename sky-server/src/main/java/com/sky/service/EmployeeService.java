6d44411e6d1a6e77c48e2e75195f73ec2ce57625
云腾致雨 <shenmafuyun213@163.com>
2025-04-23 17:01:17 +0800

    苍穹外卖day2

--- sky-server/src/main/java/com/sky/service/EmployeeService.java
+++ sky-server/src/main/java/com/sky/service/EmployeeService.java
@@ -2,7 +2,9 @@ package com.sky.service;
 
 import com.sky.dto.EmployeeDTO;
 import com.sky.dto.EmployeeLoginDTO;
+import com.sky.dto.EmployeePageQueryDTO;
 import com.sky.entity.Employee;
+import com.sky.result.PageResult;
 
 public interface EmployeeService {
 
@@ -19,4 +21,11 @@ public interface EmployeeService {
      *
      */
     void save(EmployeeDTO employeeDTO);
+
+    /**
+     * 员工分页查询
+     * @Param employeePageQueryDTO
+     * @Return PageResult
+     */
+    PageResult pageQuery(EmployeePageQueryDTO employeePageQueryDTO);
 }
