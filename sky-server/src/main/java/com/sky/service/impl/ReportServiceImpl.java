75cfede4510e366ca01e798f4fc1958fd2b83e42
云腾致雨 <shenmafuyun213@163.com>
2025-04-27 16:03:03 +0800

    下单提醒业务功能代码

---
+++ sky-server/src/main/java/com/sky/service/impl/ReportServiceImpl.java
@@ -0,0 +1,10 @@
+package com.sky.service.impl;
+
+import com.sky.service.ReportService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class ReportServiceImpl implements ReportService {
+}
