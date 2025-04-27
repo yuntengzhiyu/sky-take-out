0ae69ddd36852646013e4279b300b8b86986d9f2
云腾致雨 <shenmafuyun213@163.com>
2025-04-27 17:29:40 +0800

    营业额数据统计功能代码

--- sky-server/src/main/java/com/sky/service/impl/ReportServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/ReportServiceImpl.java
@@ -1,10 +1,54 @@
 package com.sky.service.impl;
 
+import com.sky.entity.Orders;
+import com.sky.mapper.OrderMapper;
 import com.sky.service.ReportService;
+import com.sky.vo.TurnoverReportVO;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Service
 @Slf4j
 public class ReportServiceImpl implements ReportService {
+    @Autowired
+    private OrderMapper orderMapper;
+
+
+
+    @Override
+    public TurnoverReportVO getTurnover(LocalDate beginTime, LocalDate endTime) {
+        List<LocalDate> dateList = new ArrayList<>();
+        dateList.add(beginTime);
+        while(!beginTime.equals(endTime)) {
+            beginTime = beginTime.plusDays(1);
+            dateList.add(beginTime);
+        }
+        List<Double> turnoverList = new ArrayList<>();
+        for(LocalDate date : dateList) {
+            LocalDateTime begin = LocalDateTime.of(date, LocalTime.MIN);
+            LocalDateTime end = LocalDateTime.of(date, LocalTime.MAX);
+            Map map = new HashMap();
+            map.put("begin", begin);
+            map.put("end", end);
+            map.put("status", Orders.COMPLETED);
+            Double turnover = orderMapper.sumByMap(map);
+            turnover = turnover == null ? 0 : turnover;
+            turnoverList.add(turnover);
+
+        }
+        return TurnoverReportVO.builder()
+                .dateList(StringUtils.join(dateList,","))
+                .turnoverList(StringUtils.join(turnoverList,","))
+                .build();
+    }
 }
